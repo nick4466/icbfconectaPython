253fa8516bc0d2654e6d011055210bcdb139c1ea
Yissell05-P <cathybetancourt54@gmail.com>
2025-11-26 19:51:36 -0500

    planeacion corregida con dimensiones
    ya quedo bien

--- icbfconecta/settings.py
+++ icbfconecta/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core', 
-    'planeaciones',
+   'planeaciones.apps.PlaneacionesConfig',
     'asistencia',
     'novedades',
     'desarrollo',
--- planeaciones/apps.py
+++ planeaciones/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class PlaneacionesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'planeaciones'
+
+    def ready(self):
+        import planeaciones.signals
