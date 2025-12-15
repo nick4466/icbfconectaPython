ec361818498c915172c0197e285a1816e8fea561
nick4466 <stivnieto14@gmail.com>
2025-12-15 02:01:10 -0500

    Config: Usar rutas relativas para media/ en ambos entornos

--- icbfconecta/settings.py
+++ icbfconecta/settings.py
@@ -137,15 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if IS_PRODUCTION:
-    # PythonAnywhere: rutas absolutas
-    STATIC_ROOT = '/home/NIC000/icbfconectaPython/static'
-    STATICFILES_DIRS = []
-else:
-    # Desarrollo local: rutas relativas
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# Usar rutas relativas en ambos entornos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
@@ -169,10 +163,5 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # Configuración para subir imagenes en las planeacion
 MEDIA_URL = '/media/'
-
-if IS_PRODUCTION:
-    # PythonAnywhere: rutas absolutas
-    MEDIA_ROOT = '/home/NIC000/icbfconectaPython/media'
-else:
-    # Desarrollo local: rutas relativas
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Usar siempre ruta relativa - funciona en local y en PythonAnywhere
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
